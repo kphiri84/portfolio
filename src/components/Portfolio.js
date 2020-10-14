66735f68c6bae0c41da42ace09ef4ee8cc5a4bfb
kphiri84 <kenny.phiri@laposte.net>
2020-10-14 17:31:29 +0200

    add storpe

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './portfolio.css';
+import storpe from '../assets/storpe.png'
 
 const Portfolio = () => {
 	return ( 
@@ -10,7 +11,7 @@ const Portfolio = () => {
 			<div className="container">
 				<div className="box">
 					<div className="imgBx">
-						<img className="images" src="https://zupimages.net/up/20/21/k9fl.png" alt="projet1"/>
+						<img className="images" src="https://zupimages.net/up/20/21/k9fl.png" alt="tarantino"/>
 					</div>
 					<div className="contentBx">
 						<h2>SITE TARANTINO</h2>
@@ -21,7 +22,7 @@ const Portfolio = () => {
 				</div>
 				<div className="box">
 					<div className="imgBx">
-						<img className="images" src="https://zupimages.net/up/20/22/naix.png" alt="projet1"/>
+						<img className="images" src="https://zupimages.net/up/20/22/naix.png" alt="geek trotter"/>
 					</div>
 					<div className="contentBx">
 						<h2>GEEK TROTTER</h2>
@@ -32,7 +33,7 @@ const Portfolio = () => {
 				</div>
 				<div className="box">
 					<div className="imgBx">
-						<img className="images" src="https://zupimages.net/up/20/22/x1jy.png" alt="projet1"/>
+						<img className="images" src="https://zupimages.net/up/20/22/x1jy.png" alt="memory"/>
 					</div>
 					<div className="contentBx">
 						<h2>MEMORY HORROR GAME</h2>
@@ -43,7 +44,7 @@ const Portfolio = () => {
 				</div>
 				<div className="box">
 					<div className="imgBx">
-						<img className="images" src="https://zupimages.net/up/20/37/klq2.png" alt="projet1" />
+						<img className="images" src="https://zupimages.net/up/20/37/klq2.png" alt="marche rose" />
 					</div>
 					<div className="contentBx">
 						<h2>MARCHE ROSE COMBOURG</h2>
@@ -54,7 +55,7 @@ const Portfolio = () => {
 				</div>
 				<div className="box">
 					<div className="imgBx">
-						<img className="images" src="https://zupimages.net/up/20/37/bkoo.png" alt="projet1"/>
+						<img className="images" src="https://zupimages.net/up/20/37/bkoo.png" alt="chat app"/>
 					</div>
 					<div className="contentBx">
 						<h2>CHAT APP</h2>
@@ -65,13 +66,24 @@ const Portfolio = () => {
 				</div>
 				<div className="box">
 					<div className="imgBx">
-						<img className="images" src="https://zupimages.net/up/20/37/f5mj.png" alt="projet1"/>
+						<img className="images" src="https://zupimages.net/up/20/37/f5mj.png" alt="wild circus"/>
 					</div>
 					<div className="contentBx">
 						<h2>WILD CIRCUS</h2>
 						<p>Développement d'un site sur le thème du cirque avec authentification et réservation en ligne</p>
 						<p>ReactJS/NodeJS/Express/JWT/MySql/Sequelize</p>
 						<a href="https://wild-circus-app.netlify.app/">Lien</a>
+					</div>
+				</div>
+				<div className="box">
+					<div className="imgBx">
+						<img className="images" src={storpe} alt="storpe"/>
+					</div>
+					<div className="contentBx">
+						<h2>STORPE</h2>
+						<p>Développement d'un application de championnat de pronostics sportifs web et mobile</p>
+						<p>ReactJS/NodeJS/Express/JWT/MySql/Sequelize</p>
+						<a href="https://www.storpe.club/">Lien</a>
 					</div>
 				</div>
 			</div>
